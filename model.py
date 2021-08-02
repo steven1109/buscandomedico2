1dc78236d7f05a5dd492aa6b01d977bb1190f87b
Steven Jefferson Velasquez <steven.jefferson.ve@ad.upcnet.es>
2021-08-02 15:00:06 +0200

    Habilitando los filtros del endpoint buscando medicos por especialidad

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import os
+import unicodedata
 
 
 class Dispatcher:
@@ -51,18 +51,44 @@ class Dispatcher:
             return ubigeosDict
 
         elif parameters['type'] == 'Medico':
-            query = 'select me.id_medico,me.nombres,me.ape_paterno,me.ape_materno,me.genero,me.cod_departamento,me.cod_distrito,me.cod_provincia, ' \
-                'me.codigo_cmp,me.comentario_personal,es.des_especialidad, ' \
-                'COUNT(com.id_medico) AS count_doc,  ' \
-                'CASE WHEN COUNT(com.id_medico) > 0 THEN SUM(com.puntaje) ELSE 0 END AS sum_com, ' \
-                'CASE WHEN COUNT(com.id_medico) > 0 THEN ROUND(AVG(com.puntaje),2) ELSE 0 END AS prom ' \
-                'from medico me ' \
-                'inner join especialidad_medico esme on me.id_medico = esme.id_medico ' \
-                'inner join especialidad es on esme.id_especialidad = es.id_especialidad ' \
-                'left join comentarios com on me.id_medico = com.id_medico ' \
-                'where es.bol_activo = 1 ' \
-                'group by me.id_medico,me.nombres,me.ape_paterno,me.ape_materno,me.genero,me.cod_departamento,me.cod_distrito,me.cod_provincia, ' \
-                'me.codigo_cmp,me.comentario_personal,es.des_especialidad'
+
+            # Filtros
+            clausulas = ""
+            if parameters['genero'] != "":
+                clausulas += ' AND me.genero = {}'.format(
+                    int(parameters['genero']))
+
+            if parameters['cod_departamento'] != "":
+                clausulas += ' AND me.cod_departamento = "{}"'.format(
+                    parameters['cod_departamento'])
+
+            if parameters['cod_provincia'] != "":
+                clausulas += ' AND me.cod_provincia = "{}"'.format(
+                    parameters['cod_provincia'])
+
+            if parameters['cod_distrito'] != "":
+                clausulas += ' AND me.cod_distrito = "{}"'.format(
+                    parameters['cod_distrito'])
+
+            if parameters['especialidad'] != "":
+                name_specialization = unicodedata.normalize(
+                    'NFKD', parameters['especialidad']).encode('ASCII', 'ignore').upper().decode("utf-8")
+                clausulas += ' AND es.des_especialidad LIKE "%{}%"'.format(
+                    name_specialization.upper())
+
+            query = ' select me.id_medico,me.nombres,me.ape_paterno,me.ape_materno,me.genero,me.cod_departamento,me.cod_distrito,me.cod_provincia, ' \
+                ' me.codigo_cmp,me.comentario_personal,es.des_especialidad, ' \
+                ' COUNT(com.id_medico) AS count_doc,  ' \
+                ' CASE WHEN COUNT(com.id_medico) > 0 THEN SUM(com.puntaje) ELSE 0 END AS sum_com, ' \
+                ' CASE WHEN COUNT(com.id_medico) > 0 THEN ROUND(AVG(com.puntaje),2) ELSE 0 END AS prom ' \
+                ' from medico me ' \
+                ' inner join especialidad_medico esme on me.id_medico = esme.id_medico ' \
+                ' inner join especialidad es on esme.id_especialidad = es.id_especialidad ' \
+                ' left join comentarios com on me.id_medico = com.id_medico ' \
+                ' where es.bol_activo = 1 {} ' \
+                ' group by me.id_medico,me.nombres,me.ape_paterno,me.ape_materno,me.genero,me.cod_departamento,me.cod_distrito,me.cod_provincia, ' \
+                ' me.codigo_cmp,me.comentario_personal,es.des_especialidad ' \
+                ' order by me.ape_materno;'.format(clausulas)
 
             self.cur.execute(query)
             medicos = self.cur.fetchall()
