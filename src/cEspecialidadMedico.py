fbcd75125cdbd91da809c9538cfeb0d8fe1f68fe
steven.jefferson.velasquez <steven.jefferson.velasquez@ithinkupc.com>
2021-10-27 13:04:59 +0200

    Completando los metodos CRUD para latabla especialidad_medico

--- src/cEspecialidadMedico.py
+++ src/cEspecialidadMedico.py
@@ -7,13 +7,23 @@ class EspecialidadMedico:
         self.param = parameters
 
     def add_data(self):
-        pass
+        id_especialidad = self.param['id_especialidad']
+        id_medico = self.param['id_medico']
+        codigo_rne = str(self.param['codigo_rne'])
+        creation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        query = ' INSERT INTO especialidad_medico (id_especialidad, id_medico, codigo_rne, fec_creacion) ' \
+            ' VALUES (%s, %s, %s, %s)'
+
+        values = (id_especialidad, id_medico, codigo_rne, creation_date)
+
+        return query, values
 
     def read_data(self):
         clausulas = ''
         if self.param['id_medico'] != "":
             clausulas += ' AND me.genero = {}'.format(
-                int(self.param['genero']))
+                int(self.param['id_medico']))
 
         if self.param['especialidad'] != "":
             name_specialization = unicodedata.normalize(
@@ -21,21 +31,32 @@ class EspecialidadMedico:
             clausulas += ' AND es.des_especialidad LIKE "%{}%"'.format(
                 name_specialization.upper())
 
-        query = ' select em.id_especialidad_medico, em.id_especialidad, es.des_especialidad, ' \
+        query = ' SELECT em.id_especialidad_medico, em.id_especialidad, es.des_especialidad, ' \
             ' em.id_medico, me.nombres, me.ape_paterno, me.ape_materno, em.codigo_rne, ' \
             ' em.fec_creacion, me.bol_activo ' \
-            ' from especialidad_medico em ' \
-            ' inner join especialidad es on em.id_especialidad = es.id_especialidad ' \
-            ' inner join medico me on em.id_medico = me.id_medico ' \
-            ' where me.bol_activo = 1 {};'.format(clausulas)
+            ' FROM especialidad_medico em ' \
+            ' INNER JOIN especialidad es ON em.id_especialidad = es.id_especialidad ' \
+            ' INNER JOIN medico me ON em.id_medico = me.id_medico ' \
+            ' WHERE me.bol_activo = 1 {};'.format(clausulas)
 
         return query
 
     def update_data(self):
-        pass
+        id_especialidad = self.param['id_especialidad']
+        id_medico = self.param['id_medico']
+        codigo_rne = self.param['codigo_rne']
+        modification_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        query = ' UPDATE especialidad_medico SET id_especialidad = %s, id_medico = %s, codigo_rne = %s, fec_modificacion = %s ' \
+            ' WHERE id_especialidad_medico = {};'.format(
+                self.param['id_especialidad_medico'])
+
+        values = (id_especialidad, id_medico, codigo_rne, modification_date)
+
+        return query, values
 
     def delete_data(self):
-        query = ' delete from especialidad_medico where id_especialidad_medico = {}'.format(
+        query = ' DELETE FROM especialidad_medico WHERE id_especialidad_medico = {};'.format(
             self.param['id_especialidad_medico'])
         return query
 
